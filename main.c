6843a5706e4e4c00665d1ff02fb258876530d401
Ruhamakhan123 <105592893+Ruhamakhan123@users.noreply.github.com>
2023-04-07 05:35:46 +0500

    Update main.c

--- main.c
+++ main.c
@@ -16,8 +16,8 @@ void initialize_grid(int grid[ROWS][COLS]) {
 }
 
 void print_grid(int grid[ROWS][COLS]) {
-    printf("\033[2J"); // clear the screen
-    printf("\033[%d;%dH", 0, 0); // move cursor to (0, 0)
+    printf("\033[2J"); 
+    printf("\033[%d;%dH", 0, 0); 
     for(int row=0; row<ROWS; row++) {
         for(int col=0; col<COLS; col++) {
             if(grid[row][col] == 1) {
