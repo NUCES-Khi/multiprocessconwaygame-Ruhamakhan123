cf175ec3c9cc869415a307c0acc1ced7a99acc5c
Ruhamakhan123 <105592893+Ruhamakhan123@users.noreply.github.com>
2023-04-07 05:33:37 +0500

    Update main.c

--- main.c
+++ main.c
@@ -1,32 +1,94 @@
-/**
- * @defgroup   MAIN main
- *
- * @brief      This file implements main. Please Comment you code
- *
- * @author     Enter Your name and Student ID here.
- * @date       2023
- */
-
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <semaphore.h>
-#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
+#define ROWS 10
+#define COLS 10
 
-/**
- * @brief      { function_description }
- *
- * @param[in]  argc  The count of arguments
- * @param      argv  The arguments array
- *
- * @return     { description_of_the_return_value }
- */
-int main(int argc, char const *argv[])
-{
-    // It is left up to you how you use shared memory and multiple processes 
-    // to solve the conway problem
-    return 0;
+void initialize_grid(int grid[ROWS][COLS]) {
+    for(int row=0; row<ROWS; row++) {
+        for(int col=0; col<COLS; col++) {
+            grid[row][col] = rand() % 2;
+        }
+    }
 }
+
+void print_grid(int grid[ROWS][COLS]) {
+    printf("\033[2J"); // clear the screen
+    printf("\033[%d;%dH", 0, 0); // move cursor to (0, 0)
+    for(int row=0; row<ROWS; row++) {
+        for(int col=0; col<COLS; col++) {
+            if(grid[row][col] == 1) {
+                printf("# ");
+            } else {
+                printf(". ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int count_neighbors(int grid[ROWS][COLS], int row, int col) {
+    int count = 0;
+    for(int i=row-1; i<=row+1; i++) {
+        for(int j=col-1; j<=col+1; j++) {
+            if(i < 0 || i >= ROWS || j < 0 || j >= COLS) {
+                continue;
+            } else if(i == row && j == col) {
+                continue;
+            } else if(grid[i][j] == 1) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+void update_grid(int grid[ROWS][COLS], int new_grid[ROWS][COLS]) {
+    for(int row=0; row<ROWS; row++) {
+        for(int col=0; col<COLS; col++) {
+            int neighbors = count_neighbors(grid, row, col);
+            if(grid[row][col] == 1 && (neighbors < 2 || neighbors > 3)) {
+                new_grid[row][col] = 0;
+            } else if(grid[row][col] == 0 && neighbors == 3) {
+                new_grid[row][col] = 1;
+            } else {
+                new_grid[row][col] = grid[row][col];
+            }
+        }
+    }
+}
+
+int main() {
+    int grid[ROWS][COLS];
+    int new_grid[ROWS][COLS];
+
+    srand(time(NULL));
+    initialize_grid(grid);
+    initialize_grid(new_grid);
+
+    while(1) {
+        update_grid(grid, new_grid);
+        print_grid(new_grid);
+        int pid = fork();
+        if(pid == 0){
+            for(int row=0; row<ROWS; row++) {
+                for(int col=0; col<COLS; col++) {
+                    grid[row][col] = new_grid[row][col];
+                }
+            }
+            sleep(1);
+            exit(0);
+        }else{
+            for(int row=0; row<ROWS; row++) {
+                for(int col=0; col<COLS; col++) {
+                    new_grid[row][col] = grid[row][col];
+                }
+            }
+            wait(NULL);
+        }
+    }
+    return 0;
+} 
